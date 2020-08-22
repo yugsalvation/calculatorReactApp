6f6cad7c361dbaca86fbd58eafa7922fe69735b2
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-08-23 02:22:27 +0530

    first commit

---
+++ src/components/calculator.jsx
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./calculator.css";
+class Calculator extends Component {
+  state = {};
+  styles = {};
+  inputRef = React.createRef();
+  operation = "";
+  val1 = 0;
+  val2 = 0;
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="verticalcenter"></div>
+        </div>
+        <div className="row">
+          <div className="col-sm-4"></div>
+          <div className="col-sm-4">
+            <table className="table table-dark tablealign">
+              <tbody>
+                <tr>
+                  <td colspan="3" align="center">
+                    <h1>Calculator</h1>
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="3" align="center">
+                    <div className="form-group">
+                      <label for="value"></label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="value"
+                        placeholder="enter value"
+                        ref={this.inputRef}
+                      />
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg"
+                      onClick={this.additions}
+                    >
+                      +
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg"
+                      onClick={this.subtractions}
+                    >
+                      -
+                    </button>
+                  </td>
+                  <td rowspan="2">
+                    <button
+                      type="button"
+                      class="btn btn-primary tdalign btn-lg"
+                      onClick={this.equals}
+                    >
+                      =
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg"
+                      onClick={this.multiplications}
+                    >
+                      *
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg"
+                      onClick={this.deletions}
+                    >
+                      /
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div className="col-sm-4"></div>
+        </div>
+      </div>
+    );
+  }
+  additions = () => {
+    this.val1 = Number(this.inputRef.current.value);
+    this.inputRef.current.value = "";
+    this.operation = "+";
+  };
+  subtractions = () => {
+    this.val1 = Number(this.inputRef.current.value);
+    this.inputRef.current.value = "";
+    this.operation = "-";
+  };
+  deletions = () => {
+    this.val1 = Number(this.inputRef.current.value);
+    this.inputRef.current.value = "";
+    this.operation = "/";
+  };
+  multiplications = () => {
+    this.val1 = Number(this.inputRef.current.value);
+    this.inputRef.current.value = "";
+    this.operation = "*";
+  };
+  equals = () => {
+    this.val2 = Number(this.inputRef.current.value);
+    let op = this.operation;
+    if (op === "+") {
+      this.inputRef.current.value = this.val1 + this.val2;
+    } else if (op === "-") {
+      this.inputRef.current.value = this.val1 - this.val2;
+    } else if (op === "/") {
+      this.inputRef.current.value = this.val1 / this.val2;
+    } else if (op === "*") {
+      this.inputRef.current.value = this.val1 * this.val2;
+    } else {
+      this.inputRef.current.value = this.val1;
+    }
+  };
+}
+export default Calculator;
