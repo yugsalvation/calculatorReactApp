5de19185ec209866f90f58b160857c9ffb0af014
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-09-01 10:49:24 +0530

    Updates

---
+++ precache-manifest.5a44f08ee013228f85716c382e1f463b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72725aa0e4545d12507438b6b345c40",
+    "url": "/calculatorReactApp/index.html"
+  },
+  {
+    "revision": "af41b409fbb5ad77806b",
+    "url": "/calculatorReactApp/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8a76e50097728bdf50dd",
+    "url": "/calculatorReactApp/static/css/main.44ba9843.chunk.css"
+  },
+  {
+    "revision": "af41b409fbb5ad77806b",
+    "url": "/calculatorReactApp/static/js/2.d6895e5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculatorReactApp/static/js/2.d6895e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a76e50097728bdf50dd",
+    "url": "/calculatorReactApp/static/js/main.63d7ccf2.chunk.js"
+  },
+  {
+    "revision": "fc80245403f3cbad38d9",
+    "url": "/calculatorReactApp/static/js/runtime-main.938d85bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculatorReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
